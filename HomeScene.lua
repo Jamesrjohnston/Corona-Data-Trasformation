7538597027d3f618ea9e0b6e9706989b89028b8f
James Johnston <James.johnston@iinet.net.au>
2020-09-01 20:37:33 +0800

    admin

---
+++ HomeScene.lua
@@ -0,0 +1,10 @@
+-----------------------------------------------------------------------------------------
+--
+-- HomeScene.lua
+--
+--Graphic Simulation of a Machine Learning Data Separator
+--Project: Mobile App
+--CSP2108 Introduction to Mobile Applcations Development 
+-- Sem 2 2020
+--By James Johnston - 10461776
+-----------------------------------------------------------------------------------------
---
+++ processing.lua
@@ -0,0 +1,11 @@
+-----------------------------------------------------------------------------------------
+--
+-- processing.lua
+--
+--Graphic Simulation of a Machine Learning Data Separator
+--Project: Mobile App
+--CSP2108 Introduction to Mobile Applcations Development 
+-- Sem 2 2020
+--By James Johnston - 10461776
+-- This file will handle all the processing of the data
+-----------------------------------------------------------------------------------------
