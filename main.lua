1d940a1a588fa612fbfea89e5b74e62339627a62
James Johnston <James.johnston@iinet.net.au>
2020-09-04 23:48:37 +0800

    Add convertcolor
    Add convertcolor libary

--- main.lua
+++ main.lua
@@ -10,27 +10,3 @@
 -----------------------------------------------------------------------------------------
 
 
-function loadFile(path)
-    -- Initalise table
-    local points = {}
-    -- Open the file handle
-    local file, errorString = io.open(path, 'r')
-    if not file then
-        -- Error occurred; output the cause
-        print('File error: ' .. errorString)
-    else
-        local lineIndex = 1
-        -- add each line to table
-        for line in file:lines() do
-            -- add row to table, formatted as [x, y, classifier].
-            points[lineIndex] = {}
-            points[lineIndex][1], points[lineIndex][2], points[lineIndex][3] =
-                string.match(line, '(.*),(.*),(%a*)', 1)
-            lineIndex = lineIndex + 1
-        end
-        -- Close the file handle
-        io.close(file)
-    end
-    local file = nil
-    return points
-end
